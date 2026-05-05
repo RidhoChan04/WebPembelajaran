c01163099c4021a57f16953b778ea3be880c84ea
unknown <chando150705@gmail.com>
2026-05-05 23:24:04 +0700

    deploy b4

--- server/index.js
+++ server/index.js
@@ -28,7 +28,7 @@ app.use(cors({
     methods: "GET,POST,PUT,DELETE",
     credentials: true
 }));
-app.options('*', cors()); // Izinkan semua request OPTIONS
+app.options('.*', cors()); // Izinkan semua request OPTIONS
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
